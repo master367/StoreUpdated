83b710814f030c52bcb470f39bf48b813dd2a2f2
master367 <adiko7634@gmail.com>
2025-01-08 03:11:17 +0500

    2

--- main.go
+++ main.go
@@ -5,17 +5,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/time/rate"
 	"html/template"
-	"log"
 	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 type Cigarette struct {
-	Brand string  `json:"brand,omitempty" bson:"brand,omitempty"`
-	Price float64 `json:"price,omitempty" bson:"price,omitempty"`
+	Brand    string  `json:"brand,omitempty" bson:"brand,omitempty"`
+	Price    float64 `json:"price,omitempty" bson:"price,omitempty"`
+	Type     string  `json:"type,omitempty" bson:"type,omitempty"`
+	Category string  `json:"category,omitempty" bson:"category,omitempty"`
 }
 
 type Cart struct {
@@ -24,27 +30,104 @@ type Cart struct {
 	Total float64     `json:"total,omitempty" bson:"total,omitempty"`
 }
 
+var limiter = make(map[string]*rate.Limiter)
+var limiterLock = &sync.Mutex{}
+var log = logrus.New()
 var client *mongo.Client
 var assortmentCollection *mongo.Collection
 var cartCollection *mongo.Collection
+
+func rateLimit(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Получаем IP-адрес клиента
+		ip := r.RemoteAddr
+
+		// Блокируем доступ к limiter для изменения
+		limiterLock.Lock()
+		defer limiterLock.Unlock()
+
+		// Проверяем, существует ли уже лимитер для этого IP
+		if _, exists := limiter[ip]; !exists {
+			// Если нет, создаем новый лимитер с лимитом 5 запросов в минуту
+			limiter[ip] = rate.NewLimiter(rate.Every(time.Minute), 5)
+		}
+
+		// Проверяем, может ли этот IP сделать запрос
+		if !limiter[ip].Allow() {
+			log.Printf("Rate limit exceeded for IP: %s", ip)
+			http.Error(w, "Превышен лимит запросов", http.StatusTooManyRequests)
+			return
+		}
+
+		// Если лимит не превышен, передаем управление следующему обработчику
+		next.ServeHTTP(w, r)
+	})
+}
+
+func init() {
+	file, err := os.OpenFile("logs/app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	log.SetOutput(file)
+	log.SetFormatter(&logrus.JSONFormatter{
+		TimestampFormat: time.RFC3339,
+	})
+	log.SetLevel(logrus.InfoLevel)
+}
 
 func connectToMongo() {
 	var err error
 	client, err = mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
-		log.Fatal("Failed to connect to MongoDB:", err)
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("Не удалось подключиться к MongoDB")
 	}
 	err = client.Ping(context.Background(), nil)
 	if err != nil {
-		log.Fatal("MongoDB connection is not responding:", err)
+		log.WithFields(logrus.Fields{
+			"error": err,
+		}).Fatal("MongoDB не отвечает")
 	}
-	fmt.Println("Connected to MongoDB")
+	log.Info("Успешно подключено к MongoDB")
 	assortmentCollection = client.Database("Shop").Collection("assortment")
 	cartCollection = client.Database("Shop").Collection("cart")
 }
 
-func getAllCigarettes(w http.ResponseWriter, r *http.Request) {
-	cursor, err := assortmentCollection.Find(context.Background(), bson.D{})
+func getCigarettesWithFilters(w http.ResponseWriter, r *http.Request) {
+	brand := r.URL.Query().Get("brand")
+	sortField := r.URL.Query().Get("sortField")
+	sortOrder := r.URL.Query().Get("sortOrder") // "asc" или "desc"
+	limitParam := r.URL.Query().Get("limit")
+	pageParam := r.URL.Query().Get("page")
+
+	limit := 10
+	page := 1
+	if limitParam != "" {
+		fmt.Sscanf(limitParam, "%d", &limit)
+	}
+	if pageParam != "" {
+		fmt.Sscanf(pageParam, "%d", &page)
+	}
+
+	filter := bson.M{}
+	if brand != "" {
+		filter["brand"] = bson.M{"$regex": brand, "$options": "i"} // Регистронезависимый поиск
+	}
+
+	sort := bson.D{}
+	if sortField != "" {
+		sortDirection := 1
+		if sortOrder == "desc" {
+			sortDirection = -1
+		}
+		sort = append(sort, bson.E{Key: sortField, Value: sortDirection})
+	}
+
+	skip := (page - 1) * limit
+	options := options.Find().SetSort(sort).SetLimit(int64(limit)).SetSkip(int64(skip))
+	cursor, err := assortmentCollection.Find(context.Background(), filter, options)
 	if err != nil {
 		http.Error(w, "Error fetching cigarettes", http.StatusInternalServerError)
 		return
@@ -67,21 +150,35 @@ func getAllCigarettes(w http.ResponseWriter, r *http.Request) {
 }
 
 func addCigaretteToCart(w http.ResponseWriter, r *http.Request) {
+	log.WithFields(logrus.Fields{
+		"method": r.Method,
+		"url":    r.URL.Path,
+		"ip":     r.RemoteAddr,
+	}).Info("Request received")
+
 	var cigarette Cigarette
 	err := json.NewDecoder(r.Body).Decode(&cigarette)
 	if err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
+		log.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Error("Error decoding cigarette")
+		http.Error(w, "Неверный ввод", http.StatusBadRequest)
 		return
 	}
 
+	// Добавление сигареты в корзину
 	_, err = cartCollection.InsertOne(context.Background(), cigarette)
 	if err != nil {
-		http.Error(w, "Error adding to cart", http.StatusInternalServerError)
+		log.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Error("Error adding to cart")
+		http.Error(w, "Ошибка при добавлении в корзину", http.StatusInternalServerError)
 		return
 	}
 
+	log.Info("Cigarette added to cart successfully")
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Cigarette added to cart")
+	fmt.Fprintf(w, "Сигарета добавлена в корзину")
 }
 
 func getCart(w http.ResponseWriter, r *http.Request) {
@@ -145,20 +242,62 @@ func removeItemFromCart(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Cigarette removed from cart")
 }
 
+func getCigaretteByBrand(w http.ResponseWriter, r *http.Request) {
+	brand := r.URL.Query().Get("brand")
+	var cigarette Cigarette
+	err := assortmentCollection.FindOne(context.Background(), bson.M{"brand": brand}).Decode(&cigarette)
+	if err != nil {
+		http.Error(w, "Cigarette not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(cigarette)
+}
+
+func updateCigarettePrice(w http.ResponseWriter, r *http.Request) {
+	var updateData struct {
+		Brand string  `json:"brand"`
+		Price float64 `json:"price"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&updateData)
+	if err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	_, err = assortmentCollection.UpdateOne(
+		context.Background(),
+		bson.M{"brand": updateData.Brand},
+		bson.M{"$set": bson.M{"price": updateData.Price}},
+	)
+	if err != nil {
+		http.Error(w, "Error updating price", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Price updated successfully")
+}
+
+func getLink(port int) string {
+	return "http://localhost:" + fmt.Sprintf("%d", port)
+}
+
 func main() {
 
 	connectToMongo()
 
 	r := mux.NewRouter()
-
-	r.HandleFunc("/cigarettes", getAllCigarettes).Methods("GET")
+	r.HandleFunc("/add-to-cart", addCigaretteToCart)
+	r.HandleFunc("/cigarettes", getCigarettesWithFilters).Methods("GET")
 	r.HandleFunc("/cart", getCart).Methods("GET")
-	r.HandleFunc("/cart/add", addCigaretteToCart).Methods("POST")
-	r.HandleFunc("/cart/remove", removeItemFromCart).Methods("POST") // Новый маршрут
+	r.Handle("/cart/add", rateLimit(http.HandlerFunc(addCigaretteToCart))).Methods("POST")
+	r.HandleFunc("/cart/remove", removeItemFromCart).Methods("POST")
 	r.HandleFunc("/cart/clear", clearCart).Methods("POST")
-
+	r.HandleFunc("/cigarette", getCigaretteByBrand).Methods("GET")
+	r.HandleFunc("/cigarette/update", updateCigarettePrice).Methods("POST")
 	r.HandleFunc("/", serveHTML)
 
-	log.Println("Server started on port 8080")
+	log.Printf("Server started on %s", getLink(8080))
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
